cde5694b17fa58afc6d7e3db258811488584d17a
StefanyLino <stefanylinosilva47@gmail.com>
2025-02-03 11:32:36 -0300

    Initial commit

---
+++ exemplo_chrome/script.js
@@ -0,0 +1,5 @@
+// Não é obrigatório usar";"
+console.log("Olá Fanyy")
+
+// Para criar um "pop-up" na tela com a mensagem
+alert("Aoba")
---
+++ exemplo_variavel/script.js
@@ -0,0 +1,16 @@
+// A maioria das variáveis vão ser declaradas como "LET"
+
+// Para definir variável
+let nome = ("Fanyy")
+let endereco = ("Rua das Palmeiras, 123, Bairro Jardim Aurora - São Paulo")
+// console.log(nome)
+// alert(nome)
+
+// Para buscar informação(id) no arquivo html
+// Para buscar um valor numérico usar .value
+// Para buscar um "texto" usar .innerText
+// Colocar = nome da váriavel
+
+// O "+" serve para juntar dados
+
+document.getElementById("resultado").innerText = "Olá, seu nome é " + nome + ", você mora no bairro " + endereco + ", boa tarde."
