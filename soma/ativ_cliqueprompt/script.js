73678d39ba062ee47d5d5d179a04d1b48c6e58bd
StefanyLino <stefanylinosilva47@gmail.com>
2025-02-04 11:01:31 -0300

    Prompt

---
+++ soma/ativ_cliqueprompt/script.js
@@ -0,0 +1,7 @@
+let num1 = parseInt(prompt("Digite um número"))
+let num2 = parseInt(prompt("Digite outro número"))
+let soma = num1 + num2
+
+const clique = () => {
+    window.alert (document.getElementById("resultado").innerText = soma)
+}
