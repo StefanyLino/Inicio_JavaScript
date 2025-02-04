64610deabcb15db4e63e4af9eb267d2868d5e4a2
StefanyLino <stefanylinosilva47@gmail.com>
2025-02-04 08:06:03 -0300

    script.js

--- exemplo_prompt/script.js
+++ exemplo_prompt/script.js
@@ -1,4 +1,5 @@
 let nome = prompt("Qual o seu nome?")
+
 let idade = prompt("Digite sua idade:")
 
 document.getElementById("prompt").innerText = "Olá " + nome + " você tem " + idade + " anos de idade"
