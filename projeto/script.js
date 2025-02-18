182d1321a19d2f6959e6e21e976a0d67325ec6ff
StefanyLino <stefanylinosilva47@gmail.com>
2025-02-18 11:35:24 -0300

    Script - JS

--- projeto/script.js
+++ projeto/script.js
@@ -1,3 +1,19 @@
-const aperto = () =>{
-    console.log("deu certo")
+let lista = document.querySelectorAll('.item')
+let next = document.getElementById('proximo')
+let prev = document.getElementById('anterior')
+
+let contar = lista.length
+let ativo = 0
+
+next.onclick = () => {
+    let desativar = document.querySelector('.ativo')
+    desativar.classList.remove('ativo')
 }
+
+prev.onclick = () => {
+    let desativar = document.querySelector('.ativo')
+    desativar.classList.remove('ativo')
+}
+
+// "." = pegar tudo
+// classlist é uma palavra reservada para retirar algo
