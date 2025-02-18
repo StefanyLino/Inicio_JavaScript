b0fb14b33a975372fa6e1c80cc80ca7ed43fd0c5
StefanyLino <stefanylinosilva47@gmail.com>
2025-02-18 13:17:17 -0300

    Script - HTML

--- projeto/script.js
+++ projeto/script.js
@@ -8,11 +8,24 @@ let ativo = 0
 next.onclick = () => {
     let desativar = document.querySelector('.ativo')
     desativar.classList.remove('ativo')
+    if(ativo>=contar-1){
+        ativo=0;
+    }else{
+        ativo=ativo+1
+    }
+    lista[ativo].classList.add('ativo')
 }
 
 prev.onclick = () => {
     let desativar = document.querySelector('.ativo')
     desativar.classList.remove('ativo')
+    if(ativo<=contar-3){
+        ativo= 2;
+    }else{
+        ativo=ativo-1
+    }
+    lista[ativo].classList.add('ativo')
+    
 }
 
 // "." = pegar tudo
